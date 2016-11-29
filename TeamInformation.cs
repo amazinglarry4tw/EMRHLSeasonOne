aba1902ba6c2d0d69c1efac0467c964065418120
Jeremy Gallagher <amazinglarry4tw@gmail.com>
2016-11-29 02:14:13 -0500

    TeamResources file.

---
+++ TeamInformation.cs
@@ -0,0 +1,37 @@
+SEASON ONE
+
+SABRES				CANADIENS				WHALERS					BRUINS
+John White			Derek Jones				Dave Belisle			Jay Mello
+Eric Higgins		Dan Leveilee			Tim Lewis				Shawn Mello
+Derek Sauchuk		Bill St. Pierre			George Dixon			Zac Macaro
+Gary Bamford		Brian Davis				Tom Beaulieau			James Carter
+Jay Fraser			Scott Liporto			Kurt Need				Ed LaPlante
+John Morrison		Luis Carmona			Terry Andress			Mike Noone
+Chris Spencer G		Freddy Silva G			Artie Atkinson - G		Mike Strauss G
+
+								SEASON TWO
+John White			Derek Jones				Bill St. Pierre			Jay Mello
+Eric Higgins		Dave Belisle			Jack Copley				Jay Fraser
+Dave Gardner		Tom Beaulieu			Shawn Mello				Darryl MacEachen
+Gary Bamford		Brian Davis				Steve Burdick			James Carter
+Zac Macaro			Scott Liporto			Dan Leveillee			Tim Lewis
+John Morisson		Luis Carmona 			Terry Andress			Mike Noone
+Jay Brown G			Ed LaPlante G			Chris Spencer G			Mike Strauss G
+
+								SEASON THREE
+John White			Dave Gardner			James Carter			Derek Jones
+Shawn Mello			Gary Bamford			Dave Belisle			Jay Fraser
+Bill St. Pierre		John Morrison			Mike Noone				Jay Mello
+Luis Carmona		Steve Burdick			Ed LaPlante				Tom Beaulieu
+Dan Leveillee		Terry Andress			Darryl MacEachen		Zac Macaro
+Scott Liporto		Jack Copley				Eric Higgins			John Glidden
+Jay Brown G			Chirs Spencer G			Artie Atkinson G		Mike Strauss  G
+
+								SEASON FOUR
+John White			Eric Higgins			James Carter			Derek Jones
+Jay Mello			John Glidden			Shawn Mello				Dave Gardner
+Bill St. Pierre		Zac Macaro				Scott Liporto			Dave Belisle
+Mike Noone			Ed LaPlante				Mike Strauss			Jack Copley
+Gary Bamford		Luis Carmona			Darryl MacEachen		Terry Andress
+Tom Beaulieu		Jay Fraser				Dan Leveillee			John Morrison
+Artie Atkinson G	Chris Spencer G			Freddy Silva G			Ed Goodwin  G
